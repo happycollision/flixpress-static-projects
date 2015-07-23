3e93afba8b2f0f33c0fe6346a0f12802ca0ac334
Don Denton <don@happycollision.com>
2015-07-23 12:33:39 -0600

    refines pop-over behavior to slide

--- src/js/flixpress-modal.js
+++ src/js/flixpress-modal.js
@@ -1,32 +1,46 @@
+// Do a little work as soon as possible (before page is done loading is fine):
+
 var popStyles = {
   width: '100%',
   backgroundColor: 'grey',
   height: '100%',
   position: 'fixed',
   top: 0,
-  left: 0
+  left: 0,
+  scroll: 'auto'
 };
-var $pop = $('<div class="flixpress-modal"></div>');
-var $closeButton = $('<div class="flixpress-modal-close-button">X</div>');
-
-$closeButton.css({
+var closeButtonStyles = {
   cursor: 'pointer',
   position: 'absolute',
   top: 5,
   right: 5,
   padding: '2px',
   backgroundColor: 'white'
-});
+};
+
+var $pop = $('<div class="flixpress-modal"></div>');
+var $closeButton = $('<div class="flixpress-modal-close-button">X</div>');
+
+$closeButton.css(closeButtonStyles);
 
 $pop.css(popStyles).append($closeButton).hide();
 
 $(document).ready(function(){
-  $('.modal').on('click', function(){
-    $pop.appendTo('body').show('slide');
+  // append to body ASAP, ready for action
+  $pop.appendTo('body');
 
-    // TEMPORARY
+  $('body').on('click', '.modal', function(e){
+    // stop href from working
+    e.preventDefault();
+
+    // TODO: add content to pop-over
+
+    // show pop-over
+    $pop.show('slide', {direction: 'down'});
+
+    // close pop-over
     $pop.on('click', '.flixpress-modal-close-button', function(){
-      $pop.hide('slide');
+      $pop.hide('slide', {direction: 'down'});
     });
   });
 });
