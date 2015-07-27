a098063fcfb2d8a23c4b0a69c2e6932f4f388a22
Don Denton <don@happycollision.com>
2015-07-27 16:53:41 -0600

    Wrap document to keep from polluting the global

--- src/js/flixpress-modal.js
+++ src/js/flixpress-modal.js
@@ -1,43 +1,47 @@
-// Do a little work as soon as possible (before page is done loading is fine):
+(function(){
 
-var $modalFull = $('<div class="flixpress-full-modal"></div>');
-var $modalContent = $('<div class="flixpress-modal-content"></div>');
-var $toolbar = $('<div class="flixpress-modal-toolbar"><img src="images/flix-gear.png" /></div>');
-var $closeButton = $('<div class="flixpress-modal-close-button">Close</div>');
+  // Do a little work as soon as possible (before page is done loading is fine):
 
-$toolbar.hide();
-$closeButton.prependTo($toolbar);
-$modalFull.append($modalContent).hide();
+  var $modalFull = $('<div class="flixpress-full-modal"></div>');
+  var $modalContent = $('<div class="flixpress-modal-content"></div>');
+  var $toolbar = $('<div class="flixpress-modal-toolbar"><img src="images/flix-gear.png" /></div>');
+  var $closeButton = $('<div class="flixpress-modal-close-button">Close</div>');
 
-$(document).ready(function(){
-  // append to body ASAP, ready for action
-  $modalFull.appendTo('body');
-  $toolbar.appendTo('body');
+  $toolbar.hide();
+  $closeButton.prependTo($toolbar);
+  $modalFull.append($modalContent).hide();
 
-  $('body').on('click', '.modal', function(e){
-    // stop href from working
-    e.preventDefault();
+  $(document).ready(function(){
+    // append to body ASAP, ready for action
+    $modalFull.appendTo('body');
+    $toolbar.appendTo('body');
 
-    // TODO: add real content to pop-over
-    $modalContent.html($('.modal-content').html());
+    $('body').on('click', '.modal', function(e){
+      // stop href from working
+      e.preventDefault();
 
-    // Freeze body scrolling
-    $('body').css({overflow: 'hidden'});
+      // TODO: add real content to pop-over
+      $modalContent.html($('.modal-content').html());
 
-    // show pop-over
-    $modalFull.show('slide', {direction: 'down'}, function(){
-      $toolbar.show('slide', {direction: 'up', easing: 'easeOutBounce', duration: 800});
+      // Freeze body scrolling
+      $('body').css({overflow: 'hidden'});
+
+      // show pop-over
+      $modalFull.show('slide', {direction: 'down'}, function(){
+        $toolbar.show('slide', {direction: 'up', easing: 'easeOutBounce', duration: 800});
+      });
+
+      // close pop-over
+      $('body').on('click', '.flixpress-modal-close-button', function(){
+        // Allow body scrolling again
+        $('body').css({overflow: 'auto'});
+        
+        // hide pop-over and toolbar
+        $modalFull.hide('slide', {direction: 'down'});
+        $toolbar.hide('slide', {direction: 'up'});
+      });
     });
-
-    // close pop-over
-    $('body').on('click', '.flixpress-modal-close-button', function(){
-      // Allow body scrolling again
-      $('body').css({overflow: 'auto'});
-      
-      // hide pop-over and toolbar
-      $modalFull.hide('slide', {direction: 'down'});
-      $toolbar.hide('slide', {direction: 'up'});
-    });
+    $('.modal.button').click();
   });
-  //$('.modal.button').click();
-});
+
+})();
