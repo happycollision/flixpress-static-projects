c435f2b15bbff37ecf2e5100238dd272aae5d9a2
Don Denton <don@happycollision.com>
2015-07-27 16:47:43 -0600

    rename classes and variables for semantics

--- src/js/flixpress-modal.js
+++ src/js/flixpress-modal.js
@@ -1,17 +1,17 @@
 // Do a little work as soon as possible (before page is done loading is fine):
 
-var $pop = $('<div class="flixpress-modal"></div>');
-var $popContent = $('<div class="flixpress-modal-content"></div>');
+var $modalFull = $('<div class="flixpress-full-modal"></div>');
+var $modalContent = $('<div class="flixpress-modal-content"></div>');
 var $toolbar = $('<div class="flixpress-modal-toolbar"><img src="images/flix-gear.png" /></div>');
 var $closeButton = $('<div class="flixpress-modal-close-button">Close</div>');
 
 $toolbar.hide();
 $closeButton.prependTo($toolbar);
-$pop.append($popContent).hide();
+$modalFull.append($modalContent).hide();
 
 $(document).ready(function(){
   // append to body ASAP, ready for action
-  $pop.appendTo('body');
+  $modalFull.appendTo('body');
   $toolbar.appendTo('body');
 
   $('body').on('click', '.modal', function(e){
@@ -19,13 +19,13 @@ $(document).ready(function(){
     e.preventDefault();
 
     // TODO: add real content to pop-over
-    $popContent.html($('.modal-content').html());
+    $modalContent.html($('.modal-content').html());
 
     // Freeze body scrolling
     $('body').css({overflow: 'hidden'});
 
     // show pop-over
-    $pop.show('slide', {direction: 'down'}, function(){
+    $modalFull.show('slide', {direction: 'down'}, function(){
       $toolbar.show('slide', {direction: 'up', easing: 'easeOutBounce', duration: 800});
     });
 
@@ -35,7 +35,7 @@ $(document).ready(function(){
       $('body').css({overflow: 'auto'});
       
       // hide pop-over and toolbar
-      $pop.hide('slide', {direction: 'down'});
+      $modalFull.hide('slide', {direction: 'down'});
       $toolbar.hide('slide', {direction: 'up'});
     });
   });
