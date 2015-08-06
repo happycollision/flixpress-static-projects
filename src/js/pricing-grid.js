3663ecf9a8683181a2f2f8c1488076254c427685
Don Denton <don@happycollision.com>
2015-08-06 13:03:15 -0600

    Add tooltips

--- src/js/pricing-grid.js
+++ src/js/pricing-grid.js
@@ -1,6 +1,7 @@
 var showMoreText = "Show more";
 var showLessText = "Show less";
 $(document).ready(function(){
+  // Take care of show more and show less
   var $more = $('.more-info');
   $more.html(showMoreText);
   $more.on('click', function(){
@@ -12,4 +13,24 @@ $(document).ready(function(){
     }
   });
   $more.show();
+
+  // take care of explainers
+  $('.explainer').show().click(function(e){
+    e.preventDefault();
+  });
+  $('.explainer').hover(function(){
+    var href = $(this).attr('href');
+    var html = $(href).html();
+    var explainDiv = $('<div class="explain-div"></div>');
+
+    explainDiv.hide().html(html).css({
+      position: 'absolute',
+      top: $(this).position().top + 'px',
+      left: ($(this).position().left + 20) + 'px'
+    });
+    explainDiv.appendTo($('body')).show();
+  }, function(){
+    $('.explain-div').remove();
+  });
+
 });
