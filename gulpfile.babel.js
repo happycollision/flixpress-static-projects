1cb8016559f9108b749f40f32994cc66b6bd235f
Don Denton <don@happycollision.com>
2015-10-07 17:30:41 -0500

    beginning to transition to Gulp

---
+++ gulpfile.babel.js
@@ -0,0 +1,207 @@
+// generated on 2015-10-05 using generator-gulp-webapp 1.0.3
+
+// node_modules requires:
+import gulp from 'gulp';
+import gulpLoadPlugins from 'gulp-load-plugins';
+import browserSync from 'browser-sync';
+import del from 'del';
+// import {stream as wiredep} from 'wiredep';
+
+// NodeJS level requires:
+import fs from 'fs';
+
+const $ = gulpLoadPlugins();
+const reload = browserSync.reload;
+
+// /* I am doing this slightly different than suggested at https://github.com/nkostelnik/gulp-s3
+//    With my version, only the Key and Secret are in the JSON file. I can define the
+//    bucket here.
+
+//    The aws.json file ought to look like this:
+
+//    {
+//   "key": "SOMETHING",
+//   "secret": "SOMETHING ELSE"
+//    }
+// */
+// var aws = JSON.parse(fs.readFileSync('./aws.json'));
+// aws.bucket = 'FlixSamples';
+// const awsCredentials = aws;
+// const awsOptions = {uploadPath: "development_files/Scripts/flixpress-js/"}
+
+gulp.task('styles', () => {
+  return gulp.src('src/sass/*.{scss,sass}')
+    .pipe($.plumber())
+    // .pipe($.sourcemaps.init())
+    .pipe($.compass({
+      outputStyle: 'expanded',
+      precision: 10,
+      css: '.tmp/.css',
+      sass: 'src/sass',
+      require: ['susy','breakpoint']
+    }).on('error', function(err){}))
+    .pipe($.autoprefixer({browsers: ['last 10 versions']}))
+    // .pipe($.sourcemaps.write())
+    .pipe(gulp.dest('.tmp/styles'))
+    .pipe(reload({stream: true}));
+});
+
+// function lint(files, options) {
+//   return () => {
+//     return gulp.src(files)
+//       .pipe(reload({stream: true, once: true}))
+//       .pipe($.eslint(options))
+//       .pipe($.eslint.format())
+//       .pipe($.if(!browserSync.active, $.eslint.failAfterError()));
+//   };
+// }
+// const testLintOptions = {
+//   env: {
+//     mocha: true
+//   }
+// };
+
+// gulp.task('lint', lint('app/**/*.js'));
+// gulp.task('lint:test', lint('test/spec/**/*.js', testLintOptions));
+
+// gulp.task('html', ['styles'], () => {
+//   const assets = $.useref.assets({searchPath: ['.tmp', 'app', '.']});
+
+//   return gulp.src('app/*.html')
+//     .pipe(assets)
+//     .pipe($.if('*.js', $.uglify()))
+//     .pipe($.if('*.css', $.minifyCss({compatibility: '*'})))
+//     .pipe(assets.restore())
+//     .pipe($.useref())
+//     .pipe($.if('*.html', $.minifyHtml({conditionals: true, loose: true})))
+//     .pipe(gulp.dest('dist'));
+// });
+
+// gulp.task('images', () => {
+//   return gulp.src('app/images/**/*')
+//     .pipe($.if($.if.isFile, $.cache($.imagemin({
+//       progressive: true,
+//       interlaced: true,
+//       // don't remove IDs from SVGs, they are often used
+//       // as hooks for embedding and styling
+//       svgoPlugins: [{cleanupIDs: false}]
+//     }))
+//     .on('error', function (err) {
+//       console.log(err);
+//       this.end();
+//     })))
+//     .pipe(gulp.dest('dist/images'));
+// });
+
+// gulp.task('fonts', () => {
+//   return gulp.src(require('main-bower-files')({
+//     filter: '**/*.{eot,svg,ttf,woff,woff2}'
+//   }).concat('app/fonts/**/*'))
+//     .pipe(gulp.dest('.tmp/fonts'))
+//     .pipe(gulp.dest('dist/fonts'));
+// });
+
+// gulp.task('extras', () => {
+//   return gulp.src([
+//     'app/*.*',
+//     '!app/*.html'
+//   ], {
+//     dot: true
+//   }).pipe(gulp.dest('dist'));
+// });
+
+// gulp.task('clean', del.bind(null, ['.tmp', 'dist']));
+
+// gulp.task('serve', ['styles', 'fonts'], () => {
+//   browserSync({
+//     notify: false,
+//     port: 9000,
+//     server: {
+//       baseDir: ['.tmp', 'app'],
+//       routes: {
+//         '/bower_components': 'bower_components'
+//       }
+//     }
+//   });
+
+//   gulp.watch([
+//     'app/*.html',
+//     'app/**/*.js',
+//     'app/images/**/*',
+//     '.tmp/fonts/**/*'
+//   ]).on('change', reload);
+
+//   gulp.watch('app/styles/**/*.scss', ['styles']);
+//   gulp.watch('app/fonts/**/*', ['fonts']);
+//   gulp.watch('bower.json', ['wiredep', 'fonts']);
+// });
+
+// gulp.task('serve:dist', () => {
+//   browserSync({
+//     notify: false,
+//     port: 9000,
+//     server: {
+//       baseDir: ['dist']
+//     }
+//   });
+// });
+
+// gulp.task('serve:test', () => {
+//   browserSync({
+//     notify: false,
+//     port: 9000,
+//     ui: false,
+//     server: {
+//       baseDir: 'test',
+//       routes: {
+//         '/bower_components': 'bower_components'
+//       }
+//     }
+//   });
+
+//   gulp.watch('test/spec/**/*.js').on('change', reload);
+//   gulp.watch('test/spec/**/*.js', ['lint:test']);
+// });
+
+// // inject bower components
+// gulp.task('wiredep', () => {
+//   gulp.src('app/styles/*.scss')
+//     .pipe(wiredep({
+//       ignorePath: /^(\.\.\/)+/
+//     }))
+//     .pipe(gulp.dest('app/styles'));
+
+//   gulp.src('app/*.html')
+//     .pipe(wiredep({
+//       ignorePath: /^(\.\.\/)*\.\./
+//     }))
+//     .pipe(gulp.dest('app'));
+// });
+
+// gulp.task('build', ['lint', 'html', 'images', 'fonts', 'extras'], () => {
+//   return gulp.src('dist/**/*').pipe($.size({title: 'build', gzip: true}));
+// });
+
+// gulp.task('default', ['clean'], () => {
+//   gulp.start('build');
+// });
+
+// gulp.task('requirejs', () => {
+//   return gulp.src('app/lib/flixpress.js')
+//     .pipe($.requirejsOptimize({
+//       optimize: 'none',
+//       mainConfigFile: 'app/lib/config.js',
+//       name: 'flixpress',
+//       insertRequire: ['flixpress']
+//     }))
+//     .pipe($.wrap('(function () {<%= contents %>}());'))
+//     .pipe($.addSrc.prepend('bower_components/almond/almond.js'))
+//     .pipe($.concat('flixpress.js'))
+//     .pipe($.s3(awsCredentials, awsOptions))
+//     .pipe(gulp.dest('.tmp'))
+// });
+
+// gulp.task('develop', ['requirejs'], () => {
+
+//   gulp.watch('app/**/*.js', ['requirejs']);
+// });
